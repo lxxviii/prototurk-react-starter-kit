26166af823338a416ef4bbde5755cd07e90448b0
Bora TUNÇ <lxxviii@outllook.com>
2022-11-02 19:12:16 +0300

    Test yüklemeleri yapıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import styles from './App.module.css';
+import Ref from './component/use-ref';
+import UseReducer from './component/use-reducer';
 
 function App() {
   return (
-    <div className="App">
-       hadi ya
+    <div className={styles}>
+      <div className="App">
+        <h4>{process.env.NODE_ENV}</h4>
+        <p>{process.env.REACT_APP_API_URL}</p>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <img src='../build/logo512.png' alt='' />
+          </>
+        )}
+      </div>
+      <div>
+        <Ref />
+      </div>
+      <div>
+        <UseReducer />
+      </div>
     </div>
   );
 }
-
 export default App;
